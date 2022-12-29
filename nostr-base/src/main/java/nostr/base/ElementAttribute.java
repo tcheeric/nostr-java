89c73220aa14dcb8112f8e600517eb686b45e8ff
tcheeric <tcheeric75@gmail.com>
2022-12-29 01:58:14 +0000

    Added various contructors

--- nostr-base/src/main/java/nostr/base/ElementAttribute.java
+++ nostr-base/src/main/java/nostr/base/ElementAttribute.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import nostr.types.values.IValue;
+import nostr.types.values.impl.StringValue;
 
 /**
  *
@@ -19,10 +20,24 @@ public class ElementAttribute {
     private final String name;
 
     private final IValue value;
-//    private final List valueList;
-//        
-//    @Builder.Default
-//    private final boolean isString = Boolean.TRUE;
+
     private final Integer nip;
-    
+
+    public ElementAttribute(String name, IValue value, Integer nip) {
+        this.name = name;
+        this.value = value;
+        this.nip = nip;
+    }
+
+    public ElementAttribute(String name, String value, Integer nip) {
+        this(name, new StringValue(value), nip);
+    }
+
+    public ElementAttribute(String value, Integer nip) {
+        this(null, value, nip);
+    }
+
+    public ElementAttribute(String value) {
+        this(null, value, 1);
+    }
 }
