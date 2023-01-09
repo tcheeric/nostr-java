9c9dec6085cf43079194d0aa90238c3e676fa56e
tcheeric <tcheeric75@gmail.com>
2023-01-09 22:44:25 +0000

    Added test method for the NIP-05 validator + Refactoring

--- nostr-event/src/main/java/nostr/event/impl/InternetIdentifierMetadataEvent.java
+++ nostr-event/src/main/java/nostr/event/impl/InternetIdentifierMetadataEvent.java
@@ -4,25 +4,17 @@ import nostr.base.Profile;
 import nostr.event.Kind;
 import nostr.base.PublicKey;
 import nostr.event.list.TagList;
-import nostr.json.unmarshaller.impl.JsonObjectUnmarshaller;
 import java.beans.IntrospectionException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.extern.java.Log;
 import nostr.base.annotation.Event;
+import nostr.event.util.Nip05Validator;
 import nostr.types.values.IValue;
 import nostr.types.values.impl.ExpressionValue;
 import nostr.types.values.impl.ObjectValue;
@@ -50,7 +42,9 @@ public final class InternetIdentifierMetadataEvent extends GenericEvent {
 
     @Override
     public void update() throws NoSuchAlgorithmException, IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchFieldException, NostrException {
-        this.validateNip05();
+        
+        // NIP-05 validator
+        Nip05Validator.builder().nip05(nip05).publicKey(getPubKey()).build().validate();
 
         setContent();
 
@@ -68,64 +62,9 @@ public final class InternetIdentifierMetadataEvent extends GenericEvent {
         List<ExpressionValue> expressions = new ArrayList<>();
         expressions.add(nameExpr);
         expressions.add(nip05Expr);
-        
+
         ObjectValue content = new ObjectValue(expressions);
 
         setContent(content.toString());
     }
-
-    private void validateNip05() throws NostrException {
-        if (this.nip05 != null) {
-            var localPart = nip05.split("@")[0];
-            var domain = nip05.split("@")[1];
-
-            if (!localPart.matches("^[-\\w.]+$")) {
-                throw new NostrException("Invalid <local-part> syntax in nip05 attribute.");
-            }
-
-            // Verify the public key
-            try {
-                validatePublicKey(domain, localPart);
-            } catch (IOException ex) {
-                throw new NostrException(ex);
-            }
-        }
-    }
-
-    private void validatePublicKey(String domain, String localPart) throws MalformedURLException, NostrException, IOException, ProtocolException {
-
-        // Set up and establish the HTTP connection
-        String strUrl = "https://<domain>/.well-known/nostr.json?name=<localPart>".replace("<domain>", domain).replace("<localPart>", localPart);
-        URL url = new URL(strUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-
-        // Read the connection response (1) and validate (2)
-        if (connection.getResponseCode() == 200) { // (1)
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-
-            // (2)
-            String pubKey = getPublicKey(content, localPart);
-            log.log(Level.INFO, "Public key for {0} returned by the server: [{1}]", new Object[]{localPart, pubKey});
-
-            if (!pubKey.equals(getPubKey().toString())) {
-                throw new NostrException(String.format("Public key mismatch. Expected {0} - Received: {1}", new Object[]{getPubKey().toString(), pubKey}));
-            }
-        }
-
-        throw new NostrException(String.format("Failed to connect to {0}. Error message: {1)", new Object[]{strUrl, connection.getResponseMessage()}));
-    }
-
-    private String getPublicKey(StringBuilder content, String localPart) {
-        ObjectValue jsonObjValue = new JsonObjectUnmarshaller(content.toString()).unmarshall();
-        IValue namesObj = ((ObjectValue) jsonObjValue).get("\"" + "names" + "\"");
-        IValue pubKey = ((ObjectValue) namesObj).get("\"" + localPart + "\"");
-        return pubKey.getValue().toString();
-    }
-
 }
