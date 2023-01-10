560f02fa7e195181cbe521062d908a901b39eaff
tcheeric <tcheeric75@gmail.com>
2023-01-10 23:02:42 +0000

    Add support for "escaped" json text

--- nostr-event/src/main/java/nostr/event/impl/MetadataEvent.java
+++ nostr-event/src/main/java/nostr/event/impl/MetadataEvent.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package nostr.event.impl;
 
 import nostr.base.Profile;
@@ -73,7 +69,7 @@ public final class MetadataEvent extends GenericEvent {
         value.add(picExpr);
 
         ObjectValue content = new ObjectValue(value);
-        setContent(content.toString());
+        setContent(content.toString(true));
     }
 
 }
