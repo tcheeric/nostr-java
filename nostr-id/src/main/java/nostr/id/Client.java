860ee4f78efcf91c9e030ba70ecb5ac620f08de4
Guilherme Gps <guilherme.ps20@gmail.com>
2023-04-14 16:32:34 -0300

    Improving type's marshallers for jackson

--- nostr-id/src/main/java/nostr/id/Client.java
+++ nostr-id/src/main/java/nostr/id/Client.java
@@ -74,14 +74,14 @@ public class Client {
 
                     return false;
                 }).map(fr -> {
-            try {
-                return fr.get();
-            } catch (InterruptedException | ExecutionException e) {
-                log.log(Level.SEVERE, null, e);
-            }
-
-            return null;
-        }).collect(Collectors.toSet());
+		            try {
+		                return fr.get();
+		            } catch (InterruptedException | ExecutionException e) {
+		                log.log(Level.SEVERE, null, e);
+		            }
+		
+		            return null;
+		        }).collect(Collectors.toSet());
     }
 
     public void send(@NonNull GenericMessage message) {
--- nostr-types/src/main/java/nostr/types/values/BaseValue.java
+++ nostr-types/src/main/java/nostr/types/values/BaseValue.java
@@ -1,11 +1,8 @@
 package nostr.types.values;
 
-import java.util.logging.Level;
 import lombok.Data;
 import lombok.extern.java.Log;
-import nostr.types.MarshallException;
 import nostr.types.Type;
-import nostr.types.values.marshaller.BaseTypesMarshaller;
 
 /**
  *
@@ -25,15 +22,6 @@ public abstract class BaseValue implements IValue {
 
     @Override
     public String toString() {
-        return toString(false);
-    }
-
-    public String toString(boolean escape) {
-        try {
-            return BaseTypesMarshaller.Factory.create(this, escape).marshall();
-        } catch (MarshallException ex) {
-            log.log(Level.SEVERE, null, ex);
-            throw new RuntimeException(ex);
-        }
+        return value.toString();
     }
 }
