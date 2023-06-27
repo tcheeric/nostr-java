a3f1a90305582b77f36b96a2735b9013f0787d00
tcheeric <tchepane@hey.com>
2023-06-28 00:26:57 +0100

    When using the "e" tag on the "feature/feature_bug_fixes" branch, an exception occurs if the marker string is in lowercase. #49

--- nostr-java-event/src/main/java/nostr/event/Marker.java
+++ nostr-java-event/src/main/java/nostr/event/Marker.java
@@ -1,4 +1,3 @@
-
 package nostr.event;
 
 /**
@@ -8,14 +7,27 @@ package nostr.event;
 public enum Marker {
     ROOT("root"),
     REPLY("reply");
-    
+
     private final String value;
 
     Marker(String value) {
         this.value = value;
     }
-    
+
     public String getValue() {
         return value;
+    }
+
+    public static Marker fromValue(String value) {
+        if (value == null) {
+            return null;
+        }
+        if (value.equalsIgnoreCase("root")) {
+            return ROOT;
+        } else if (value.equalsIgnoreCase("reply")) {
+            return REPLY;
+        } else {
+            throw new IllegalArgumentException("Invalid marker value: " + value);
+        }
     }
 }
