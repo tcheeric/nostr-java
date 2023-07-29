306e221ea220e4e695295c0456475f2e02d2fd60
Eric <tchepane@tuta.io>
2023-07-29 02:38:52 +0100

    Implement NIP-15 - Nostr Marketplace #72

---
+++ nostr-java-event/src/main/java/nostr/event/AbstractContent.java
@@ -0,0 +1,23 @@
+package nostr.event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nostr.base.IEvent;
+
+/**
+ *
+ * @author eric
+ * @param <T>
+ */
+public abstract class AbstractContent<T extends IEvent> implements IContent {
+        
+    @Override
+    public String toString() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
