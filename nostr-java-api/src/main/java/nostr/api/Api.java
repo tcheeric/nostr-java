f705f011f7e14b1aff2375de9933c13014d411e4
Eric <tchepane@tuta.io>
2023-07-29 03:19:43 +0100

    Nostradamic vs Nostr-java #71

--- nostr-java-api/src/main/java/nostr/api/Api.java
+++ nostr-java-api/src/main/java/nostr/api/Api.java
@@ -7,7 +7,9 @@ package nostr.api;
 import lombok.NonNull;
 import nostr.base.GenericTagQuery;
 import nostr.base.IEvent;
+import nostr.base.ISignable;
 import nostr.base.Relay;
+import nostr.base.Signature;
 import nostr.client.Client;
 import nostr.event.BaseEvent;
 import nostr.event.BaseMessage;
@@ -22,6 +24,7 @@ import nostr.event.json.codec.BaseTagEncoder;
 import nostr.event.json.codec.FiltersDecoder;
 import nostr.event.json.codec.FiltersEncoder;
 import nostr.event.json.codec.GenericTagQueryEncoder;
+import nostr.id.Identity;
 import nostr.util.NostrException;
 
 /**
@@ -29,12 +32,17 @@ import nostr.util.NostrException;
  * @author eric
  */
 public abstract class Api {
-    
+
     public static void send(@NonNull IEvent event) {
-        var client = Client.getInstance();
+        var client = createClient();
         client.send(event);
     }
-    
+
+    public static Signature sign(@NonNull ISignable signable) throws NostrException {
+        var identity = Identity.getInstance();
+        return identity.sign(signable);
+    }
+
     // Events
     public static String toJson(@NonNull BaseEvent event) throws NostrException {
         return Api.toJson(event, null);
@@ -44,13 +52,12 @@ public abstract class Api {
         final var enc = new BaseEventEncoder(event, relay);
         return enc.encode();
     }
-    
+
     public static BaseEvent fromJsonEvent(@NonNull String json) throws NostrException {
         final var dec = new BaseEventDecoder(json);
         return dec.decode();
     }
-    
-    
+
     // Messages
     public static String toJson(@NonNull BaseMessage message, Relay relay) throws NostrException {
         final var enc = new BaseMessageEncoder(message, relay);
@@ -60,13 +67,12 @@ public abstract class Api {
     public static String toJson(@NonNull BaseMessage message) throws NostrException {
         return Api.toJson(message, null);
     }
-    
+
     public static BaseMessage fromJsonMessage(@NonNull String json) throws NostrException {
         final var dec = new BaseMessageDecoder(json);
         return dec.decode();
     }
-    
-    
+
     // Tags
     public static String toJson(@NonNull BaseTag tag, Relay relay) throws NostrException {
         final var enc = new BaseTagEncoder(tag, relay);
@@ -76,13 +82,12 @@ public abstract class Api {
     public static String toJson(@NonNull BaseTag tag) throws NostrException {
         return Api.toJson(tag, null);
     }
-    
+
     public static BaseTag fromJsonTag(@NonNull String json) throws NostrException {
         final var dec = new BaseTagDecoder(json);
         return dec.decode();
     }
-    
-    
+
     // Filters
     public static String toJson(@NonNull Filters filters, Relay relay) throws NostrException {
         final var enc = new FiltersEncoder(filters, relay);
@@ -92,13 +97,12 @@ public abstract class Api {
     public static String toJson(@NonNull Filters filters) throws NostrException {
         return Api.toJson(filters, null);
     }
-    
+
     public static Filters fromJsonFilters(@NonNull String json) throws NostrException {
         final var dec = new FiltersDecoder(json);
         return dec.decode();
-    }    
-    
-    
+    }
+
     // Generic Tag Queries
     public static String toJson(@NonNull GenericTagQuery gtq, Relay relay) throws NostrException {
         final var enc = new GenericTagQueryEncoder(gtq, relay);
@@ -107,5 +111,21 @@ public abstract class Api {
 
     public static String toJson(@NonNull GenericTagQuery gtq) throws NostrException {
         return Api.toJson(gtq, null);
-    }    
+    }
+
+    // Utils
+    private static Client createClient() {
+        final var client = Client.getInstance();
+
+        do {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        } while (client.getThreadPool().getCompletedTaskCount() < (client.getRelays().size() / 2));
+
+        return client;
+    }
+
 }
