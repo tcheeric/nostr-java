d924a5678bb4c9c5de112f63cab428689ab7768e
Eric <tchepane@tuta.io>
2023-08-13 10:50:40 +0100

    Nostradamic vs Nostr-java #71

---
+++ nostr-java-base/src/main/java/nostr/base/NIP28ContentMetadata.java
@@ -0,0 +1,27 @@
+package nostr.base;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ *
+ * @author eric
+ */
+@Data
+@AllArgsConstructor
+public class NIP28ContentMetadata {
+
+    private final String reason;
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException ex) {
+            return null;
+        }
+    }
+}
