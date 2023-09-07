6d177877ea1b0217c2e97bdc6139ae0fc4b3c578
Eric <tchepane@tuta.io>
2023-09-07 08:08:46 +0100

    Refactoring

--- nostr-java-api/src/main/java/nostr/api/NIP09.java
+++ nostr-java-api/src/main/java/nostr/api/NIP09.java
@@ -4,11 +4,14 @@
  */
 package nostr.api;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.NonNull;
 import nostr.api.factory.impl.NIP09.DeletionEventFactory;
 import nostr.event.BaseTag;
 import nostr.event.impl.DeletionEvent;
+import nostr.event.impl.GenericEvent;
+import nostr.event.tag.EventTag;
 
 /**
  *
@@ -17,11 +20,26 @@ import nostr.event.impl.DeletionEvent;
 public class NIP09 extends Nostr {
 
     /**
-     * Create a NIP09 Deletion Event  
+     * Create a NIP09 Deletion Event
+     *
      * @param tags list of event or address tags to be deleted
      * @return the deletion event
      */
     public static DeletionEvent createDeletionEvent(@NonNull List<BaseTag> tags) {
+        return new DeletionEventFactory(tags).create();
+    }
+
+    /**
+     * Create a NIP09 Deletion Event
+     *
+     * @param event the (single) event to delete
+     * @return the deletion event
+     */
+    public static DeletionEvent createDeletionEvent(@NonNull GenericEvent event) {
+        EventTag tag = new EventTag(event.getId());
+        List<BaseTag> tags = new ArrayList<>();
+        tags.add(tag);
+
         return new DeletionEventFactory(tags).create();
     }
 }
