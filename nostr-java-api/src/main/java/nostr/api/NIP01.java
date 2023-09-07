20d23ad2ec1ebdef0db19b7c587a4a0e8cad2345
Eric <tchepane@tuta.io>
2023-09-07 08:11:42 +0100

    Refactoring

--- nostr-java-api/src/main/java/nostr/api/NIP01.java
+++ nostr-java-api/src/main/java/nostr/api/NIP01.java
@@ -18,11 +18,13 @@ import nostr.api.factory.impl.NIP01.ReqMessageFactory;
 import nostr.api.factory.impl.NIP01.TextNoteEventFactory;
 import nostr.base.IEvent;
 import nostr.base.PublicKey;
+import nostr.base.Relay;
 import nostr.base.UserProfile;
 import nostr.event.BaseTag;
 import nostr.event.Marker;
 import nostr.event.impl.Filters;
 import nostr.event.impl.MetadataEvent;
+import nostr.event.impl.ParameterizedReplaceableEvent;
 import nostr.event.impl.TextNoteEvent;
 import nostr.event.list.EventList;
 import nostr.event.list.GenericTagQueryList;
@@ -33,7 +35,9 @@ import nostr.event.message.EoseMessage;
 import nostr.event.message.EventMessage;
 import nostr.event.message.NoticeMessage;
 import nostr.event.message.ReqMessage;
+import nostr.event.tag.AddressTag;
 import nostr.event.tag.EventTag;
+import nostr.event.tag.IdentifierTag;
 import nostr.event.tag.PubKeyTag;
 
 /**
@@ -44,15 +48,17 @@ public class NIP01 extends Nostr {
 
     /**
      * Create a NIP01 text note event without tags
+     *
      * @param content the content of the note
      * @return the text note without tags
      */
     public static TextNoteEvent createTextNoteEvent(@NonNull String content) {
         return new TextNoteEventFactory(content).create();
     }
-    
+
     /**
      * Create a NIP01 text note event with tags
+     *
      * @param tags the note tags
      * @param content the content of the note
      * @return a text note event
@@ -60,10 +66,10 @@ public class NIP01 extends Nostr {
     public static TextNoteEvent createTextNoteEvent(@NonNull List<BaseTag> tags, @NonNull String content) {
         return new TextNoteEventFactory(tags, content).create();
     }
-    
-    
+
     /**
      * Create a NIP01 metadata event
+     *
      * @param profile the associated profile
      * @return a metadata event associated for the profile
      */
@@ -73,42 +79,50 @@ public class NIP01 extends Nostr {
 
     /**
      * Create a NIP01 event tag
-     * @param relateEvent the related event
+     *
+     * @param relateEventId the related event id
      * @return an event tag with the id of the related event
      */
-    public static EventTag createEventTag(@NonNull IEvent relateEvent) {
-        return new EventTagFactory(relateEvent).create();        
+    public static EventTag createEventTag(@NonNull String relateEventId) {
+        return new EventTagFactory(relateEventId).create();
     }
-    
+
     /**
      * Create a NIP01 event tag with additional recommended relay and marker
-     * @param relateEvent the related event
+     *
+     * @param relateEventId the related event id
      * @param recommendedRelayUrl the recommended relay
      * @param marker the marker
-     * @return an event tag with the id of the related event and optional recommended relay and marker
+     * @return an event tag with the id of the related event and optional
+     * recommended relay and marker
      */
-    public static EventTag createEventTag(@NonNull IEvent relateEvent, String recommendedRelayUrl, Marker marker) {
-        var result = new EventTagFactory(relateEvent).create();
+    public static EventTag createEventTag(@NonNull String relateEventId, String recommendedRelayUrl, Marker marker) {
+        var result = new EventTagFactory(relateEventId).create();
         result.setMarker(marker);
         result.setRecommendedRelayUrl(recommendedRelayUrl);
         return result;
     }
-    
+
     /**
      * Create a NIP01 pubkey tag
+     *
      * @param publicKey the associated public key
-     * @return a pubkey tag with the hex representation of the associated public key
+     * @return a pubkey tag with the hex representation of the associated public
+     * key
      */
     public static PubKeyTag createPubKeyTag(@NonNull PublicKey publicKey) {
         return new PubKeyTagFactory(publicKey).create();
     }
 
     /**
-     * Create a NIP01 pubkey tag with additional recommended relay and petname (as defined in NIP02)
+     * Create a NIP01 pubkey tag with additional recommended relay and petname
+     * (as defined in NIP02)
+     *
      * @param publicKey the associated public key
      * @param mainRelayUrl the recommended relay
      * @param petName the petname
-     * @return a pubkey tag with the hex representation of the associated public key and the optional recommended relay and petname
+     * @return a pubkey tag with the hex representation of the associated public
+     * key and the optional recommended relay and petname
      */
     public static PubKeyTag createPubKeyTag(@NonNull PublicKey publicKey, String mainRelayUrl, String petName) {
         var result = new PubKeyTagFactory(publicKey).create();
@@ -119,13 +133,19 @@ public class NIP01 extends Nostr {
 
     /**
      * Create a NIP01 filters object (all parameters are optional)
-     * @param events a list of event 
-     * @param authors a list of pubkeys or prefixes, the pubkey of an event must be one of these
+     *
+     * @param events a list of event
+     * @param authors a list of pubkeys or prefixes, the pubkey of an event must
+     * be one of these
      * @param kinds a list of a kind numbers
-     * @param referencedEvents a list of event ids that are referenced in an "e" tag
-     * @param referencePubKeys a list of pubkeys that are referenced in a "p" tag
-     * @param since an integer unix timestamp in seconds, events must be newer than this to pass
-     * @param until an integer unix timestamp in seconds, events must be older than this to pass
+     * @param referencedEvents a list of event ids that are referenced in an "e"
+     * tag
+     * @param referencePubKeys a list of pubkeys that are referenced in a "p"
+     * tag
+     * @param since an integer unix timestamp in seconds, events must be newer
+     * than this to pass
+     * @param until an integer unix timestamp in seconds, events must be older
+     * than this to pass
      * @param limit maximum number of events to be returned in the initial query
      * @param genericTagQueryList a generic tag query list
      * @return a filters object
@@ -143,21 +163,23 @@ public class NIP01 extends Nostr {
         factory.setUntil(until);
         return factory.create();
     }
-    
+
     /**
-     * Create an event message to send events requested by clients 
+     * Create an event message to send events requested by clients
+     *
      * @param event the related event
      * @param subscriptionId the related subscription id
-     * @return an event message  
+     * @return an event message
      */
     public static EventMessage createEventMessage(@NonNull IEvent event, @NonNull String subscriptionId) {
         var result = new EventMessageFactory(event).create();
         result.setSubscriptionId(subscriptionId);
         return result;
     }
-    
+
     /**
      * Create a REQ message to request events and subscribe to new updates
+     *
      * @param subscriptionId the subscription id
      * @param filters the filters object
      * @return a REQ message
@@ -165,9 +187,10 @@ public class NIP01 extends Nostr {
     public static ReqMessage createReqMessage(@NonNull String subscriptionId, @NonNull Filters filters) {
         return new ReqMessageFactory(subscriptionId, filters).create();
     }
-    
+
     /**
      * Create a CLOSE message to stop previous subscriptions
+     *
      * @param subscriptionId the subscription id
      * @return a CLOSE message
      */
@@ -176,7 +199,9 @@ public class NIP01 extends Nostr {
     }
 
     /**
-     * Create an EOSE message to indicate the end of stored events and the beginning of events newly received in real-time
+     * Create an EOSE message to indicate the end of stored events and the
+     * beginning of events newly received in real-time
+     *
      * @param subscriptionId the subscription id
      * @return an EOSE message
      */
@@ -185,11 +210,55 @@ public class NIP01 extends Nostr {
     }
 
     /**
-     * Create a NOTICE message to send human-readable error messages or other things to clients.
+     * Create a NOTICE message to send human-readable error messages or other
+     * things to clients.
+     *
      * @param message the human-readable message to send to the client
      * @return a NOTICE message
      */
     public static NoticeMessage createNoticeMessage(@NonNull String message) {
         return new NoticeMessageFactory(message).create();
+    }
+
+    /**
+     *
+     * @param kind
+     * @param comment
+     * @return
+     */
+    public static ParameterizedReplaceableEvent createParameterizedReplaceableEvent(@NonNull Integer kind, String comment) {
+        return NIP33.createParameterizedReplaceableEvent(kind, comment);
+    }
+
+    /**
+     *
+     * @param tags
+     * @param kind
+     * @param comment
+     * @return
+     */
+    public static ParameterizedReplaceableEvent createParameterizedReplaceableEvent(@NonNull List<BaseTag> tags, @NonNull Integer kind, String comment) {
+        return NIP33.createParameterizedReplaceableEvent(tags, kind, comment);
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public static IdentifierTag createIdentifierTag(@NonNull String id) {
+        return NIP33.createIdentifierTag(id);
+    }
+
+    /**
+     *
+     * @param kind
+     * @param publicKey
+     * @param idTag
+     * @param relay
+     * @return
+     */
+    public static AddressTag createAddressTag(@NonNull Integer kind, @NonNull PublicKey publicKey, @NonNull IdentifierTag idTag, Relay relay) {
+        return NIP33.createAddressTag(kind, publicKey, idTag, relay);
     }
 }
