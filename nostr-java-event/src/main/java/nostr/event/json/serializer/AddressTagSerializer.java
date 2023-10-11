03164c7277af4c9f8308b48f07a0f58c19ca391b
erict875 <erict875@tuta.io>
2023-10-12 00:08:29 +0100

    Relay.java improvements #85

--- nostr-java-event/src/main/java/nostr/event/json/serializer/AddressTagSerializer.java
+++ nostr-java-event/src/main/java/nostr/event/json/serializer/AddressTagSerializer.java
@@ -23,7 +23,7 @@ public class AddressTagSerializer extends JsonSerializer<AddressTag> {
         }
         
         if (value.getRelay() != null) {
-            jsonGenerator.writeString("," + value.getRelay().getUri());
+            jsonGenerator.writeString("," + value.getRelay().getHostname());
         }
         jsonGenerator.writeEndArray();
     }
