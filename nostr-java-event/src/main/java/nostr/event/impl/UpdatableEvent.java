578062922375b8788b4566076e19cc855e660954
nick avlo <avlo@yahoo.com>
2024-03-23 13:38:25 -0700

    nip33 replaced and simplified in NIP-01

--- nostr-java-event/src/main/java/nostr/event/impl/UpdatableEvent.java
+++
@@ -1,5 +0,0 @@
-package nostr.event.impl;
-
-public interface UpdatableEvent {
-  void update();
-}
--- nostr-java-event/src/main/java/nostr/event/impl/ValidatableEvent.java
+++
@@ -1,5 +0,0 @@
-package nostr.event.impl;
-
-public interface ValidatableEvent {
-  void validate();
-}
