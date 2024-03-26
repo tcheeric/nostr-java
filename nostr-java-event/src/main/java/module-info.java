a5d246d1f119a0b29e79d0249ac03d6b273fb560
kuiperanon <kuiperbeltanon@gmail.com>
2024-03-25 21:05:10 -0500

    Substitute openbeans for java.beans to support Android

--- nostr-java-event/src/main/java/module-info.java
+++ nostr-java-event/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module nostr.event {
     requires nostr.util;
     requires java.logging;
     requires java.desktop;
+    requires openbeans;
     
     exports nostr.event;
     exports nostr.event.impl;
