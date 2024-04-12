d436638dcd1a41e721e0d1ac379c4b1cdd7379e7
erict875 <erict875@tuta.io>
2024-04-12 20:59:57 +0100

    Use ThreadPool in the ThreadUtil class #136

--- nostr-java-util/src/main/java/nostr/util/thread/ThreadUtil.java
+++ nostr-java-util/src/main/java/nostr/util/thread/ThreadUtil.java
@@ -8,12 +8,11 @@ import nostr.context.Context;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
-
-import static java.util.concurrent.Executors.newSingleThreadExecutor;
 
 @AllArgsConstructor
 @Builder
@@ -23,27 +22,38 @@ public class ThreadUtil<T extends Task> {
     private final T task;
 
     @Builder.Default
-    private final boolean blocking = false;
+    private int timeoutSeconds = TIMEOUT_SECONDS;
+
+    @Builder.Default
+    private boolean blocking = false;
 
     public static final int TIMEOUT_SECONDS = 5;
 
+    public ThreadUtil(@NonNull T task) {
+        this.task = task;
+        this.blocking = false;
+        this.timeoutSeconds = TIMEOUT_SECONDS;
+    }
+
     public void run(@NonNull Context context) {
         log.log(Level.FINE, "Executing thread on {0}...", task);
-        ExecutorService executor = newSingleThreadExecutor();
-        Future<?> future = executor.submit(() -> {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        Future<?> futureTask = threadPool.submit(() -> {
             task.execute(context);
         });
 
         if (blocking) {
             try {
                 log.log(Level.FINE, "Waiting for thread to complete... ");
-                future.get(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+                futureTask.get(timeoutSeconds, TimeUnit.SECONDS); // Wait for the thread to complete
                 log.log(Level.FINE, "Thread execution completed!");
             } catch (InterruptedException | ExecutionException e) {
                 throw new RuntimeException(e);
             } catch (TimeoutException e) {
                 log.log(Level.WARNING, "Timeout occurred while waiting for thread to complete! Aborting...");
                 throw new RuntimeException(e);
+            } finally {
+                threadPool.shutdown();
             }
         }
     }
