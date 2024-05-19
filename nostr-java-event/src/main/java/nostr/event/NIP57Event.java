1aee7b49fbb3b69342f062c7823c1e3e5ec7a890
nick avlo <avlo@yahoo.com>
2024-05-19 10:55:01 -0700

    culled unused abstract class NIP57Event constructors updated abstract class NIP57Event constructors req'd params amount type from Integer to Long (larger range for millisats) updated serializers

--- nostr-java-event/src/main/java/nostr/event/NIP57Event.java
+++ nostr-java-event/src/main/java/nostr/event/NIP57Event.java
@@ -8,16 +8,7 @@ import java.util.List;
 
 @NoArgsConstructor
 public abstract class NIP57Event extends GenericEvent {
-
-  public NIP57Event(PublicKey pubKey, Kind kind, List<BaseTag> tags) {
-    super(pubKey, kind, tags);
-  }
-
   public NIP57Event(PublicKey pubKey, Kind kind, List<BaseTag> tags, String content) {
     super(pubKey, kind, tags, content);
-  }
-
-  public NIP57Event(PublicKey sender, Integer kind, List<BaseTag> tags, String content) {
-    super(sender, kind, tags, content);
   }
 }
