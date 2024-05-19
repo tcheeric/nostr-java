6f3df784b7095b04e3dbf7e2c1226dfc7dc5a5af
nick avlo <avlo@yahoo.com>
2024-05-19 11:50:56 -0700

    completed ZapReqestEvent factory.  ZapResponseEvent coming next

--- nostr-java-api/src/main/java/nostr/api/factory/impl/NIP57Impl.java
+++ nostr-java-api/src/main/java/nostr/api/factory/impl/NIP57Impl.java
@@ -8,9 +8,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import nostr.api.factory.EventFactory;
+import nostr.base.PublicKey;
 import nostr.event.BaseTag;
 import nostr.event.impl.ZapRequestEvent;
 import nostr.event.impl.ZapRequestEvent.ZapRequest;
+import nostr.event.tag.RelaysTag;
 import nostr.id.Identity;
 
 import java.util.List;
@@ -21,14 +23,25 @@ public class NIP57Impl {
   @EqualsAndHashCode(callSuper = false)
   public static class ZapRequestEventFactory extends EventFactory<ZapRequestEvent> {
     private final ZapRequest zapRequest;
-    public ZapRequestEventFactory(@NonNull Identity sender, List<BaseTag> tags, @NonNull String content, ZapRequest zapRequest) {
+    private final PublicKey recipientKey;
+
+    public ZapRequestEventFactory(@NonNull Identity sender, @NonNull PublicKey recipientPubKey, List<BaseTag> tags, String content, @NonNull ZapRequest zapRequest) {
       super(sender, tags, content);
       this.zapRequest = zapRequest;
+      this.recipientKey = recipientPubKey;
+    }
+
+    public ZapRequestEventFactory(@NonNull Identity sender, @NonNull PublicKey recipientPubKey, List<BaseTag> tags, String content, @NonNull Long amount, @NonNull String lnUrl, @NonNull List<String> relaysTags) {
+      this(sender, recipientPubKey, tags, content, new ZapRequest(new RelaysTag(relaysTags), amount, lnUrl));
+    }
+
+    public ZapRequestEventFactory(@NonNull Identity sender, @NonNull PublicKey recipientPubKey, List<BaseTag> tags, String content, @NonNull Long amount, @NonNull String lnUrl, @NonNull String... relaysTags) {
+      this(sender, recipientPubKey, tags, content, amount, lnUrl, List.of(relaysTags));
     }
 
     @Override
     public ZapRequestEvent create() {
-      return new ZapRequestEvent(getSender(), getTags(), getContent(), zapRequest);
+      return new ZapRequestEvent(getSender(), recipientKey, getTags(), getContent(), zapRequest);
     }
   }
 
