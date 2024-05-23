19e01b3a805f27f78047893156b92f43e1c3bb97
nick avlo <avlo@yahoo.com>
2024-05-22 19:18:30 -0700

    ongoing

---
+++ nostr-java-event/src/main/java/nostr/event/json/serializer/ClassifiedListingSerializer.java
@@ -0,0 +1,28 @@
+package nostr.event.json.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import nostr.event.tag.ClassifiedListing;
+
+import java.io.IOException;
+
+public class ClassifiedListingSerializer extends JsonSerializer<ClassifiedListing> {
+  private final PriceTagSerializer priceTagSerializer;
+
+  public ClassifiedListingSerializer() {
+    this.priceTagSerializer = new PriceTagSerializer();
+  }
+
+  @Override
+  public void serialize(ClassifiedListing classifiedListing, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+    jsonGenerator.writeStartObject();
+    jsonGenerator.writeStringField("id", classifiedListing.getId());
+    jsonGenerator.writeStringField("title", classifiedListing.getTitle());
+    jsonGenerator.writeStringField("summary", classifiedListing.getSummary());
+    jsonGenerator.writeNumberField("publishedAt", classifiedListing.getPublishedAt());
+    jsonGenerator.writeStringField("location", classifiedListing.getLocation());
+    priceTagSerializer.serialize(classifiedListing.getPriceTag(), jsonGenerator, serializerProvider);
+    jsonGenerator.writeEndObject();
+  }
+}
