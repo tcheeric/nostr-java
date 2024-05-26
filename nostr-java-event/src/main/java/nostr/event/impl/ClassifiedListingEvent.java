15c2ef77a7c73d057b83ddaa6fdc6b53e2e2f534
nick avlo <avlo@yahoo.com>
2024-05-25 17:29:18 -0700

    merge local branch

--- nostr-java-event/src/main/java/nostr/event/impl/ClassifiedListingEvent.java
+++ nostr-java-event/src/main/java/nostr/event/impl/ClassifiedListingEvent.java
@@ -1,18 +1,14 @@
 package nostr.event.impl;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import nostr.base.PublicKey;
 import nostr.base.annotation.Event;
-import nostr.event.AbstractEventContent;
 import nostr.event.BaseTag;
 import nostr.event.Kind;
 import nostr.event.NIP99Event;
-import nostr.event.json.serializer.ClassifiedListingSerializer;
 import nostr.event.tag.PriceTag;
 
 import java.math.BigDecimal;
@@ -24,7 +20,6 @@ import java.util.List;
 public class ClassifiedListingEvent extends NIP99Event {
   @JsonIgnore
   private final ClassifiedListing classifiedListing;
-
   public ClassifiedListingEvent(@NonNull PublicKey sender, @NonNull Kind kind, List<BaseTag> baseTags, String content, @NonNull ClassifiedListing classifiedListing) {
     super(sender, kind, baseTags, content);
     this.classifiedListing = classifiedListing;
@@ -40,35 +35,5 @@ public class ClassifiedListingEvent extends NIP99Event {
 
   public ClassifiedListingEvent(@NonNull PublicKey sender, List<BaseTag> baseTags, String content, @NonNull String title, @NonNull String summary, @NonNull BigDecimal number, @NonNull String currency, @NonNull String frequency) {
     this(sender, Kind.CLASSIFIED_LISTING, baseTags, content, new ClassifiedListing(title, summary, new PriceTag(number, currency, frequency)));
-  }
-
-  @Data
-  @EqualsAndHashCode(callSuper = false)
-  @JsonSerialize(using = ClassifiedListingSerializer.class)
-  public static class ClassifiedListing extends AbstractEventContent<ClassifiedListingEvent> {
-    @JsonProperty
-    private String id;
-
-    @JsonProperty
-    private String title;
-
-    @JsonProperty
-    private String summary;
-
-    @JsonProperty("published_at")
-    @EqualsAndHashCode.Exclude
-    private Long publishedAt;
-
-    @JsonProperty
-    private String location;
-
-    @JsonProperty("price")
-    private PriceTag priceTag;
-
-    public ClassifiedListing(@NonNull String title, @NonNull String summary, @NonNull PriceTag priceTag) {
-      this.title = title;
-      this.summary = summary;
-      this.priceTag = priceTag;
-    }
   }
 }
--- nostr-java-event/src/main/java/nostr/event/tag/PriceTag.java
+++ nostr-java-event/src/main/java/nostr/event/tag/PriceTag.java
@@ -1,14 +1,13 @@
 package nostr.event.tag;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import nostr.base.annotation.Tag;
 import nostr.event.BaseTag;
-import nostr.event.json.serializer.PriceTagSerializer;
 
 import java.math.BigDecimal;
 
@@ -18,9 +17,12 @@ import java.math.BigDecimal;
 @Tag(code = "price", nip = 99)
 @RequiredArgsConstructor
 @JsonPropertyOrder({"number", "currency", "frequency"})
-@JsonSerialize(using = PriceTagSerializer.class)
+//@JsonSerialize(using = PriceTagSerializer.class)
 public class PriceTag extends BaseTag {
+  @JsonProperty
   private final BigDecimal number;
+  @JsonProperty
   private final String currency;
+  @JsonProperty
   private final String frequency;
 }
