99610fd14340ddeb362658c64feaaba5e0c30650
nick avlo <avlo@yahoo.com>
2024-06-01 14:58:27 -0700

    ongoing, KindList and ProxyEventList refactored

--- nostr-java-event/src/main/java/nostr/event/list/KindList.java
+++ nostr-java-event/src/main/java/nostr/event/list/KindList.java
@@ -1,23 +1,23 @@
-package nostr.event.list;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.NonNull;
-import nostr.base.FNostrList;
-import nostr.event.json.deserializer.CustomKindListDeserializer;
-
-import java.util.List;
-
-@JsonDeserialize(using = CustomKindListDeserializer.class)
-public class KindList extends FNostrList<Integer> {
-    public KindList() {
-        super();
-    }
-
-    public KindList(Integer... kinds) {
-        this(List.of(kinds));
-    }
-
-    public KindList(@NonNull List<Integer> list) {
-        super.addAll(list);
-    }
-}
+//package nostr.event.list;
+//
+//import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+//import lombok.NonNull;
+//import nostr.base.FNostrList;
+//import nostr.event.json.deserializer.CustomKindListDeserializer;
+//
+//import java.util.List;
+//
+//@JsonDeserialize(using = CustomKindListDeserializer.class)
+//public class KindList extends FNostrList<Integer> {
+//    public KindList() {
+//        super();
+//    }
+//
+//    public KindList(Integer... kinds) {
+//        this(List.of(kinds));
+//    }
+//
+//    public KindList(@NonNull List<Integer> list) {
+//        super.addAll(list);
+//    }
+//}
