23a0e97a0688b543a65bb24566e32254ffff8429
nick avlo <avlo@yahoo.com>
2024-06-01 23:59:17 -0700

    preserve formatting

--- nostr-java-command-provider/src/main/java/module-info.java
+++ nostr-java-command-provider/src/main/java/module-info.java
@@ -17,9 +17,8 @@ module nostr.command.provider {
     requires nostr.context;
     requires nostr.context.impl;
     requires nostr.command.handler;
-  requires com.fasterxml.jackson.core;
 
-  exports nostr.command.provider;
+    exports nostr.command.provider;
 
     provides CommandHandler with
             OkCommandHandler,
