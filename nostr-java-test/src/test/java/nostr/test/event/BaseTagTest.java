0b3df7220d64ef8594ee40454bab49bf0227037f
nick avlo <avlo@yahoo.com>
2024-06-06 16:47:32 -0700

    PubkeyTag getSupportedFields() unit test

--- nostr-java-test/src/test/java/nostr/test/event/BaseTagTest.java
+++ nostr-java-test/src/test/java/nostr/test/event/BaseTagTest.java
@@ -1,7 +1,10 @@
 package nostr.test.event;
 
+import lombok.SneakyThrows;
+import nostr.base.PublicKey;
 import nostr.event.BaseTag;
 import nostr.event.impl.GenericTag;
+import nostr.event.tag.PubKeyTag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +15,14 @@ class BaseTagTest {
   @Test
   void getNip() {
     assertEquals(1, genericTag.getNip());
+  }
+
+  @SneakyThrows
+  @Test
+  void getSupportedFields() {
+    String sha256 = "56adf01ca1aa9d6f1c35953833bbe6d99a0c85b73af222e6bd305b51f2749f6f";
+    PubKeyTag pubKeyTag = new PubKeyTag(new PublicKey(sha256));
+    assertEquals(sha256, pubKeyTag.getFieldValue(pubKeyTag.getSupportedFields().stream().findFirst().orElseThrow()));
   }
 
   @Test
