5d6f8ecab62d5f7bb58f713c524a0c40ee12d7de
Jon Austen <austenjt@gmail.com>
2024-10-25 17:00:28 -0700

    Updates to unit tests. Formatting, junit5 style assertions

--- nostr-java-test/src/test/java/nostr/test/event/PriceTagTest.java
+++ nostr-java-test/src/test/java/nostr/test/event/PriceTagTest.java
@@ -11,13 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PriceTagTest {
-  @Test
-  void getSupportedFields() {
-    PriceTag priceTag = new PriceTag(new BigDecimal(11111), "BTC", "NANOSECONDS");
-    assertDoesNotThrow(() -> {
-      List<Field> list = priceTag.getSupportedFields().stream().toList();
-      assertTrue(List.of("number", "currency", "frequency").containsAll(list.stream().map(Field::getName).toList()));
-      assertTrue(List.of("java.math.BigDecimal", "java.lang.String").containsAll(list.stream().map(field -> field.getAnnotatedType().toString()).toList()));
-    });
-  }
+
+    @Test
+    void getSupportedFields() {
+        PriceTag priceTag = new PriceTag(new BigDecimal(11111), "BTC", "NANOSECONDS");
+        assertDoesNotThrow(() -> {
+            List<Field> list = priceTag.getSupportedFields().stream().toList();
+            assertTrue(List.of("number", "currency", "frequency").containsAll(list.stream().map(Field::getName).toList()));
+            assertTrue(List.of("java.math.BigDecimal", "java.lang.String").containsAll(list.stream().map(field -> field.getAnnotatedType().toString()).toList()));
+        });
+    }
+
 }
