5ee8e94805d2e46a112d5aff8a2b4fee2d1585cc
nick avlo <avlo@yahoo.com>
2025-02-01 22:36:49 -0500

    filters in own test directory

--- nostr-java-test/src/test/java/nostr/test/event/filter/FiltersTest.java
+++ nostr-java-test/src/test/java/nostr/test/event/filter/FiltersTest.java
@@ -1,4 +1,4 @@
-package nostr.test.event;
+package nostr.test.event.filter;
 
 import lombok.extern.java.Log;
 import nostr.base.GenericTagQuery;
@@ -56,10 +56,10 @@ public class FiltersTest {
 
   @Test
   public void testKindFilters() {
-    Filters filters1 = new Filters();
+    Filters filters = new Filters();
     List<Kind> kindList = List.of(Kind.CONTACT_LIST, Kind.DELETION);
-    filters1.setKinds(kindList);
-    List<Kind> kinds = filters1.getKinds();
+    filters.setKinds(kindList);
+    List<Kind> kinds = filters.getKinds();
     assertEquals(kinds, kindList);
   }
 
@@ -110,19 +110,19 @@ public class FiltersTest {
   @Test
   public void testSinceFilters() {
     Filters filters = new Filters();
-    long startTime = Date.from(Instant.now()).getTime();
-    filters.setSince(startTime);
+    long sinceTime = Date.from(Instant.now()).getTime();
+    filters.setSince(sinceTime);
 
-    assertEquals(startTime, filters.getSince());
+    assertEquals(sinceTime, filters.getSince());
   }
 
   @Test
   public void testUntilFilters() {
     Filters filters = new Filters();
-    long startTime = Date.from(Instant.now()).getTime();
-    filters.setUntil(startTime);
+    long untilTime = Date.from(Instant.now()).getTime();
+    filters.setUntil(untilTime);
 
-    assertEquals(startTime, filters.getUntil());
+    assertEquals(untilTime, filters.getUntil());
   }
 
   @Test
@@ -152,6 +152,88 @@ public class FiltersTest {
 
     Map<String, List<String>> genericTagQuery1 = filters.getGenericTagQuery(key);
     log.info(genericTagQuery1.entrySet().toString());
+
+    assertTrue(genericTagQuery1.get(key).contains(kind));
+    assertTrue(genericTagQuery1.get(key).contains(hexString));
+    assertTrue(genericTagQuery1.get(key).contains(uuid));
+    assertTrue(genericTagQuery1.get(key).contains(relayUri));
+  }
+
+  @Test
+  public void testMultipleFilters() {
+    PublicKey publicKey = Identity.generateRandomIdentity().getPublicKey();
+    GenericEvent instance = EntityFactory.Events.createTextNoteEvent(publicKey);
+    instance.update();
+
+    GenericEvent eventList = new GenericEvent();
+    eventList.setId(instance.getId());
+
+    List<Kind> kindList = List.of(Kind.CONTACT_LIST, Kind.DELETION);
+
+    GenericEvent eventContainingEventToReference = EntityFactory.Events.createTextNoteEvent(publicKey);
+    eventContainingEventToReference.setTags(List.of(new EventTag(instance.getId())));
+    eventContainingEventToReference.setTags(List.of(new PubKeyTag(instance.getPubKey())));
+    eventContainingEventToReference.update();
+
+    long sinceTime = Date.from(Instant.now()).getTime();
+    long untilTime = Date.from(Instant.now()).getTime();
+
+    Filters filters = new Filters();
+    filters.setEvents(List.of(eventList));
+    filters.setAuthors(List.of(publicKey));
+    filters.setKinds(kindList);
+    filters.setReferencedEvents(List.of(eventContainingEventToReference));
+    filters.setSince(sinceTime);
+    filters.setUntil(untilTime);
+
+    GenericTagQuery genericTagQuery = new GenericTagQuery();
+    String key = "#a";
+    genericTagQuery.setTagName(key);
+
+    String kind = Kind.TEXT_NOTE.toString();
+    String hexString = publicKey.toHexString();
+    String uuid = new IdentifierTag("uuid").getId();
+    String relayUri = new Relay("ws://localhost:5555").getUri();
+
+    List<String> addressTagValues = List.of(
+        kind,
+        hexString,
+        uuid,
+        relayUri
+    );
+    genericTagQuery.setValue(addressTagValues);
+
+    filters.setGenericTagQuery(
+        genericTagQuery.getTagName(),
+        addressTagValues);
+
+    List<GenericEvent> filtersEvents = filters.getEvents();
+    assertEquals(filtersEvents.getFirst().getId(), eventList.getId());
+
+    List<PublicKey> authors = filters.getAuthors();
+    assertEquals(authors, List.of(publicKey));
+
+    List<Kind> kinds = filters.getKinds();
+    assertEquals(kinds, kindList);
+
+    filters.getReferencedEvents().getFirst().getTags().stream()
+        .filter(EventTag.class::isInstance)
+        .map(EventTag.class::cast)
+        .findFirst()
+        .ifPresent(eventTag ->
+            assertEquals(eventTag.getIdEvent(), instance.getId()));
+
+    filters.getReferencedEvents().getFirst().getTags().stream()
+        .filter(PubKeyTag.class::isInstance)
+        .map(PubKeyTag.class::cast)
+        .findFirst()
+        .ifPresent(pubkeyTag ->
+            assertEquals(pubkeyTag.getPublicKey().toHexString(), instance.getPubKey().toHexString()));
+
+    assertEquals(sinceTime, filters.getSince());
+    assertEquals(untilTime, filters.getUntil());
+
+    Map<String, List<String>> genericTagQuery1 = filters.getGenericTagQuery(key);
 
     assertTrue(genericTagQuery1.get(key).contains(kind));
     assertTrue(genericTagQuery1.get(key).contains(hexString));
