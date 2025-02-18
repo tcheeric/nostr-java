58275201bd631839d61f62938103ec28879143a0
nick avlo <avlo@yahoo.com>
2025-02-18 14:00:47 -0800

    ongoing filters

--- nostr-java-event/src/main/java/nostr/event/json/codec/FiltersEncoder.java
+++ nostr-java-event/src/main/java/nostr/event/json/codec/FiltersEncoder.java
@@ -1,15 +1,10 @@
 package nostr.event.json.codec;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.SneakyThrows;
 import nostr.base.FEncoder;
 import nostr.event.filter.Filters;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
@@ -20,21 +15,17 @@ public class FiltersEncoder implements FEncoder<Filters> {
     this.filters = filters;
   }
 
-  @SneakyThrows
   @Override
   public String encode() {
-    List<ObjectNode> result = new ArrayList<>();
+    ObjectNode root = MAPPER.createObjectNode();
 
     filters.getFiltersMap().forEach((key, filterableList) -> {
       final ObjectNode objectNode = MAPPER.createObjectNode();
-      ObjectNode list = filterableList.stream().map(filterable -> filterable.toObjectNode(objectNode)).toList().getFirst();
-      result.add(list);
+      root.setAll(
+          filterableList.stream().map(
+              filterable -> filterable.toObjectNode(objectNode)).findFirst().orElseThrow());
     });
 
-    ObjectMapper mapper = new ObjectMapper();
-    ObjectNode root = mapper.createObjectNode();
-    result.forEach(root::setAll);
-    System.out.println(root.toPrettyString());
     return root.toString();
   }
 }
