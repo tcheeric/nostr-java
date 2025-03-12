7fc7ed8bbd83a393fd6ab40deac0a0c39a618df3
nick avlo <avlo@yahoo.com>
2025-03-11 18:14:46 -0700

    spring boot conversion, removes need for module-info.java files.  add gradle support

--- nostr-java-encryption/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module nostr.encryption {
-    requires lombok;
-    requires nostr.util;
-    requires nostr.crypto;
-    exports nostr.encryption;
-}
--- nostr-java-util/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module nostr.util {
-    requires static lombok;
-    requires java.logging;
-
-    exports nostr.util;
-    exports nostr.util.thread;
-}
