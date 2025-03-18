58aaf737047e4d36c349bb0b2be9c28ba1fff46e
nick avlo <avlo@yahoo.com>
2025-03-18 01:09:14 -0700

    maven dependency consolidation

---
+++ nostr-java-crypto/src/test/java/nostr/crypto/CryptoFailTest.java
@@ -0,0 +1,12 @@
+package nostr.crypto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Fail.fail;
+
+public class CryptoFailTest {
+	@Test
+	public void testEntityFactory() {
+		fail();
+	}
+}
