a96b4a93ee333a53da51829a347de84b0bf4e08b
nick avlo <avlo@yahoo.com>
2025-03-21 15:16:04 -0700

    BaseTagEncoder as record (removes need for lombok), TagSerializer refactor

--- nostr-java-event/src/main/java/nostr/event/json/serializer/TagSerializer.java
+++ nostr-java-event/src/main/java/nostr/event/json/serializer/TagSerializer.java
@@ -3,72 +3,45 @@ package nostr.event.json.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import nostr.base.ElementAttribute;
 import nostr.event.BaseTag;
 import nostr.event.impl.GenericTag;
 import nostr.util.NostrException;
+import org.apache.commons.lang3.stream.Streams;
 
 import java.io.IOException;
 import java.io.Serial;
-import java.lang.reflect.Field;
 import java.util.List;
 
-import static nostr.event.json.codec.BaseTagEncoder.BASETAGENCODER_MAPPED_AFTERBURNER;
+import static nostr.event.json.codec.BaseTagEncoder.BASETAG_ENCODER_MAPPED_AFTERBURNER;
 
-/**
- * @author guilhermegps
- *
- */
 public class TagSerializer extends StdSerializer<BaseTag> {
 
-    @Serial
-    private static final long serialVersionUID = -3877972991082754068L;
+	@Serial
+	private static final long serialVersionUID = -3877972991082754068L;
 
-    public TagSerializer() {
-        super(BaseTag.class);
-    }
+	public TagSerializer() {
+		super(BaseTag.class);
+	}
 
-    @Override
-    public void serialize(BaseTag value, JsonGenerator gen, SerializerProvider serializers) {
-        try {
-            // -- Create the node
-            final ObjectNode node = BASETAGENCODER_MAPPED_AFTERBURNER.getNodeFactory().objectNode();
-            List<Field> fields = value.getSupportedFields();
+	@Override
+	public void serialize(BaseTag value, JsonGenerator gen, SerializerProvider serializers) {
+		try {
+			final ObjectNode node = BASETAG_ENCODER_MAPPED_AFTERBURNER.getNodeFactory().objectNode();
+			Streams.failableStream(value.getSupportedFields().stream()).forEach(f -> node.put(f.getName(), value.getFieldValue(f)));
 
-            // Populate the node with the fields data
-            fields.forEach((Field f) -> {
-                try {
-                    node.put(f.getName(), value.getFieldValue(f));
-                } catch (NostrException ex) {
-                    throw new RuntimeException(ex);
-                }
-            });
+			if (value instanceof GenericTag genericTag) {
+				List<ElementAttribute> attrs = genericTag.getAttributes();
+				attrs.forEach(a -> node.put(a.getName(), a.getValue().toString()));
+			}
 
-            // Populate the node with the attributes data
-            if (value instanceof GenericTag genericTag) {
-                List<ElementAttribute> attrs = genericTag.getAttributes();
-                attrs.forEach(a -> node.put(a.getName(), a.getValue().toString()));
-            }
-
-            // Extract the property values from the node and serialize them as an array
-            if (node.isObject()) {
-                ArrayNode arrayNode = node.objectNode().putArray("values");
-
-                // Add the tag code as the first element
-                arrayNode.add(value.getCode());
-                node.fields().forEachRemaining(entry -> arrayNode.add(entry.getValue().asText()));
-
-                gen.writePOJO(arrayNode);
-            } else {
-                throw new AssertionError("node.isObject()", new RuntimeException());
-            }
-
-        } catch (IOException | NostrException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+			ArrayNode arrayNode = node.objectNode().putArray("values").add(value.getCode());
+			node.fields().forEachRemaining(entry -> arrayNode.add(entry.getValue().asText()));
+			gen.writePOJO(arrayNode);
+		} catch (IOException | NostrException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
