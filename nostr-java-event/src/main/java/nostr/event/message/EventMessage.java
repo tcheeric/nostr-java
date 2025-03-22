ace31b8087decb04ac12c28ced3a6def22c403e8
nick avlo <avlo@yahoo.com>
2025-03-21 21:17:18 -0700

    EventMessage refactor & cleanup

--- nostr-java-event/src/main/java/nostr/event/message/EventMessage.java
+++ nostr-java-event/src/main/java/nostr/event/message/EventMessage.java
@@ -3,7 +3,6 @@ package nostr.event.message;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -14,13 +13,18 @@ import nostr.event.BaseMessage;
 import nostr.event.impl.GenericEvent;
 import nostr.event.json.codec.BaseEventEncoder;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 import static nostr.base.Encoder.ENCODER_MAPPED_AFTERBURNER;
 import static nostr.base.IDecoder.I_DECODER_MAPPER_AFTERBURNER;
 
 @Setter
 @Getter
 public class EventMessage extends BaseMessage {
+    private static final int SIZE_JSON_EVENT_wo_SIG_ID = 2;
+    private static final Function<Object[], Boolean> isEventWoSig = (objArr) ->
+            Objects.equals(SIZE_JSON_EVENT_wo_SIG_ID, objArr.length);
 
     @JsonProperty
     private final IEvent event;
@@ -42,36 +46,30 @@ public class EventMessage extends BaseMessage {
     public String encode() throws JsonProcessingException {
         var arrayNode = getArrayNode().add(getCommand());
         Optional.ofNullable(getSubscriptionId())
-          .ifPresent(arrayNode::add);
+            .ifPresent(arrayNode::add);
         arrayNode.add(ENCODER_MAPPED_AFTERBURNER.readTree(
-          new BaseEventEncoder<>((BaseEvent)getEvent()).encode()));
+            new BaseEventEncoder<>((BaseEvent) getEvent()).encode()));
         return ENCODER_MAPPED_AFTERBURNER.writeValueAsString(arrayNode);
     }
 
-    //    TODO: refactor into stream returning optional
-    public static <T extends BaseMessage> T decode(@NonNull String jsonString) throws JsonProcessingException {
-        Object[] msgArr = I_DECODER_MAPPER_AFTERBURNER.readValue(jsonString, Object[].class);
-        var arg = msgArr[1];
-        if (msgArr.length == 2 && arg instanceof Map map) {
-            return (T) new EventMessage(
-              convertValue(I_DECODER_MAPPER_AFTERBURNER, map)
-            );
+    public static <T extends BaseMessage> T decode(@NonNull String jsonString) {
+        try {
+            Object[] msgArr = I_DECODER_MAPPER_AFTERBURNER.readValue(jsonString, Object[].class);
+            return isEventWoSig.apply(msgArr) ? processEvent(msgArr[1]) : processEvent(msgArr);
+        } catch (Exception e) {
+            throw new AssertionError("Invalid argument: " + jsonString);
         }
-
-        if (msgArr.length == 3 && arg instanceof String) {
-            if (msgArr[2] instanceof Map map) {
-                return (T) new EventMessage(
-                  convertValue(I_DECODER_MAPPER_AFTERBURNER, map),
-                  arg.toString()
-                );
-            }
-        }
-
-        throw new AssertionError("Invalid argument: " + arg);
     }
 
-    private static GenericEvent convertValue(ObjectMapper mapper, Map map) {
-        return mapper.convertValue(map, new TypeReference<GenericEvent>() {
-        });
+    public static <T extends BaseMessage> T processEvent(Object o) {
+        return (T) new EventMessage(convertValue((Map<String, String>) o));
+    }
+
+    public static <T extends BaseMessage> T processEvent(Object[] msgArr) {
+        return (T) new EventMessage(convertValue((Map<String, String>) msgArr[2]), msgArr[1].toString());
+    }
+
+    private static GenericEvent convertValue(Map<String, String> map) {
+        return nostr.base.IDecoder.I_DECODER_MAPPER_AFTERBURNER.convertValue(map, new TypeReference<>() {});
     }
 }
