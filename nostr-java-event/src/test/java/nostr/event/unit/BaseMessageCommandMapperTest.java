351c9a40f70660fa8fd36d0a5d0223c3c6495fc5
nick avlo <avlo@yahoo.com>
2025-03-26 02:40:07 -0700

    add failed test assertion

--- nostr-java-event/src/test/java/nostr/event/unit/BaseMessageCommandMapperTest.java
+++ nostr-java-event/src/test/java/nostr/event/unit/BaseMessageCommandMapperTest.java
@@ -7,66 +7,93 @@ import nostr.event.json.codec.BaseMessageDecoder;
 import nostr.event.message.EoseMessage;
 import nostr.event.message.ReqMessage;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Log
 public class BaseMessageCommandMapperTest {
-//  TODO: flesh out remaining commands
-  public final static String REQ_JSON =
-      "[\"REQ\", " +
-          "\"npub17x6pn22ukq3n5yw5x9prksdyyu6ww9jle2ckpqwdprh3ey8qhe6stnpujh\", " +
-          "{\"kinds\": [1], " +
-          "\"authors\": [\"f1b419a95cb0233a11d431423b41a42734e7165fcab16081cd08ef1c90e0be75\"]," +
-          "\"#e\": [\"fc7f200c5bed175702bd06c7ca5dba90d3497e827350b42fc99c3a4fa276a712\"]}]";
+    //  TODO: flesh out remaining commands
+    public final static String REQ_JSON =
+        "[\"REQ\", " +
+            "\"npub17x6pn22ukq3n5yw5x9prksdyyu6ww9jle2ckpqwdprh3ey8qhe6stnpujh\", " +
+            "{\"kinds\": [1], " +
+            "\"authors\": [\"f1b419a95cb0233a11d431423b41a42734e7165fcab16081cd08ef1c90e0be75\"]," +
+            "\"#e\": [\"fc7f200c5bed175702bd06c7ca5dba90d3497e827350b42fc99c3a4fa276a712\"]}]";
 
-  @Test
-  public void testReqMessageDecoder() throws JsonProcessingException {
-    log.info("testReqMessageDecoder");
+    public final static String INVALID_COMMAND_JSON =
+        "[\"OTHER\", " +
+            "\"npub17x6pn22ukq3n5yw5x9prksdyyu6ww9jle2ckpqwdprh3ey8qhe6stnpujh\", " +
+            "{\"kinds\": [1], " +
+            "\"authors\": [\"f1b419a95cb0233a11d431423b41a42734e7165fcab16081cd08ef1c90e0be75\"]," +
+            "\"#e\": [\"fc7f200c5bed175702bd06c7ca5dba90d3497e827350b42fc99c3a4fa276a712\"]}]";
 
-    BaseMessage decode = new BaseMessageDecoder<>().decode(REQ_JSON);
-    assertInstanceOf(ReqMessage.class, decode);
-  }
+    @Test
+    void testReqMessageDecoder() throws JsonProcessingException {
+        log.info("testReqMessageDecoder");
 
-  @Test
-  public void testReqMessageDecoderType() {
-    log.info("testReqMessageDecoderType");
+        BaseMessage decode = new BaseMessageDecoder<>().decode(REQ_JSON);
+        assertInstanceOf(ReqMessage.class, decode);
+    }
 
-    assertDoesNotThrow(() -> {
-      new BaseMessageDecoder<ReqMessage>().decode(REQ_JSON);
-    });
+    @Test
+    void testReqMessageDecoderType() {
+        log.info("testReqMessageDecoderType");
 
-    assertDoesNotThrow(() -> {
-      ReqMessage reqMessage = new BaseMessageDecoder<ReqMessage>().decode(REQ_JSON);
-    });
-  }
+        assertDoesNotThrow(() -> {
+            new BaseMessageDecoder<ReqMessage>().decode(REQ_JSON);
+        });
 
-  @Test
-  public void testReqMessageDecoderThrows() {
-    log.info("testReqMessageDecoderThrows");
+        assertDoesNotThrow(() -> {
+            ReqMessage reqMessage = new BaseMessageDecoder<ReqMessage>().decode(REQ_JSON);
+        });
+    }
 
-    assertThrows(ClassCastException.class, () -> {
-      EoseMessage decode = new BaseMessageDecoder<EoseMessage>().decode(REQ_JSON);
-    });
-  }
+    @Test
+    void testReqMessageDecoderThrows() {
+        log.info("testReqMessageDecoderThrows");
 
-  @Test
-  public void testReqMessageDecoderDoesNotThrow() {
-    log.info("testReqMessageDecoderDoesNotThrow");
+        assertThrows(ClassCastException.class, () -> {
+            EoseMessage decode = new BaseMessageDecoder<EoseMessage>().decode(REQ_JSON);
+        });
+    }
 
-    assertDoesNotThrow(() -> {
-      new BaseMessageDecoder<EoseMessage>().decode(REQ_JSON);
-    });
-  }
+    @Test
+    void testReqMessageDecoderDoesNotThrow() {
+        log.info("testReqMessageDecoderDoesNotThrow");
 
-  @Test
-  public void testReqMessageDecoderThrows3() {
-    log.info("testReqMessageDecoderThrows");
+        assertDoesNotThrow(() -> {
+            new BaseMessageDecoder<EoseMessage>().decode(REQ_JSON);
+        });
+    }
 
-    assertThrows(ClassCastException.class, () -> {
-      EoseMessage decode = new BaseMessageDecoder<EoseMessage>().decode(REQ_JSON);
-    });
-  }
+    @Test
+    void testReqMessageDecoderThrows3() {
+        log.info("testReqMessageDecoderThrows");
+
+        assertThrows(ClassCastException.class, () -> {
+            EoseMessage decode = new BaseMessageDecoder<EoseMessage>().decode(REQ_JSON);
+        });
+    }
+
+    @Test
+    void testInvalidMessageDecoder() {
+        log.info("testInvalidMessageDecoder");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BaseMessageDecoder<EoseMessage>().decode(INVALID_COMMAND_JSON);
+        });
+    }
+
+//    @Test
+//    void assertionFail() {
+//        assertEquals(1, 2);
+//    }
+    
+//    @Test
+//    void catastrophicTestFail() {
+//        log.info("makeSureIntegrationTestsFailhere");
+//
+//        assertDoesNotThrow(() -> {
+//            new BaseMessageDecoder<EoseMessage>().decode(INVALID_COMMAND_JSON);
+//        });
+//    }
 }
