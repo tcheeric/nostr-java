02fe19bb7f85a71490e5beaf86ba10aa9602a236
erict875 <erict875@tuta.io>
2025-05-18 05:45:06 +0100

    Refactor Kind class: update package structure, rename constants, and enhance event categorization

--- nostr-java-base/src/main/java/nostr/base/Kind.java
+++ nostr-java-base/src/main/java/nostr/base/Kind.java
@@ -1,4 +1,4 @@
-package nostr.event;
+package nostr.base;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
@@ -29,15 +29,16 @@ public enum Kind {
     MUTE_USER(44, "mute_user"),
     ENCRYPTED_PAYLOADS(44, "encrypted_payloads"),
     OTS_EVENT(1040, "ots_event"),
-    VOTE(2112, "vote"),
     RESERVED_CASHU_WALLET_TOKENS(7_374, "reserved_cashu_wallet_tokens"),
+    WALLET(17_375, "wallet"),
     WALLET_UNSPENT_PROOF(7_375, "wallet_unspent_proof"),
     WALLET_TX_HISTORY(7_376, "wallet_tx_history"),
     ZAP_REQUEST(9734, "zap_request"),
     ZAP_RECEIPT(9735, "zap_receipt"),
     REPLACEABLE_EVENT(10_000, "replaceable_event"),
-    PIN_LIST(10_001, "pin_list"),
     EPHEMEREAL_EVENT(20_000, "ephemereal_event"),
+    ADDRESSABLE_EVENT(30_000, "addressable_event"),
+    PIN_LIST(10_001, "pin_list"),
     CLIENT_AUTH(22_242, "authentication_of_clients_to_relays"),
     STALL_CREATE_OR_UPDATE(30_017, "create_or_update_stall"),
     PRODUCT_CREATE_OR_UPDATE(30_018, "create_or_update_product"),
@@ -47,8 +48,8 @@ public enum Kind {
     CLASSIFIED_LISTING_DRAFT(30_403, "classified_listing_draft"),
     CALENDAR_DATE_BASED_EVENT(31_922, "calendar_date_based_event"),
     CALENDAR_TIME_BASED_EVENT(31_923, "calendar_time_based_event"),
-    CALENDAR_RSVP_EVENT(31_925, "calendar_rsvp_event"),
-    WALLET(37_375, "wallet");
+    CALENDAR_EVENT(31_924, "calendar_event"),
+    CALENDAR_RSVP_EVENT(31_925, "calendar_rsvp_event");
 
     @JsonValue
     private final int value;
