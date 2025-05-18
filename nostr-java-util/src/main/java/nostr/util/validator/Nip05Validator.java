235bd00a27409b58be91c88bcd7f14a050e41cdd
erict875 <erict875@tuta.io>
2025-05-18 05:44:22 +0100

    Refactor Nip05Validator class: update package structure, improve public key handling, and enhance JSON processing

--- nostr-java-util/src/main/java/nostr/util/validator/Nip05Validator.java
+++ nostr-java-util/src/main/java/nostr/util/validator/Nip05Validator.java
@@ -1,11 +1,14 @@
-package nostr.event.util;
+package nostr.util.validator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.java.Log;
-import nostr.base.PublicKey;
-import nostr.event.Nip05Content;
-import nostr.event.json.codec.Nip05ContentDecoder;
 import nostr.util.NostrException;
 
 import java.io.BufferedReader;
@@ -23,19 +26,20 @@ import java.util.logging.Level;
  * @author squirrel
  */
 @Builder
+@RequiredArgsConstructor
 @Data
 @Log
 public class Nip05Validator {
 
     private final String nip05;
-    private final PublicKey publicKey;
-    
+    private final String publicKey;
+
     private static final String LOCAL_PART_PATTERN = "^[a-zA-Z0-9-_\\.]+$";
 
-//    TODO: refactor
+    //    TODO: refactor
     public void validate() throws NostrException {
         if (this.nip05 != null) {
-        	var splited = nip05.split("@");
+            var splited = nip05.split("@");
             var localPart = splited[0];
             var domain = splited[1];
 
@@ -48,13 +52,13 @@ public class Nip05Validator {
                 log.log(Level.FINE, "Validating {0}@{1}", new Object[]{localPart, domain});
                 validatePublicKey(domain, localPart);
             } catch (IOException | URISyntaxException ex) {
-            	log.log(Level.SEVERE, ex.getMessage());
+                log.log(Level.SEVERE, ex.getMessage());
                 throw new NostrException(ex);
             }
         }
     }
 
-//    TODO: refactor    
+    //    TODO: refactor
     private void validatePublicKey(String domain, String localPart) throws NostrException, IOException, URISyntaxException {
 
         // Set up and estgetPublicKeyablish the HTTP connection
@@ -76,7 +80,7 @@ public class Nip05Validator {
             String pubKey = getPublicKey(content, localPart);
             log.log(Level.FINE, "Public key for {0} returned by the server: [{1}]", new Object[]{localPart, pubKey});
 
-            if (pubKey != null && !pubKey.equals(publicKey.toString())) {
+            if (pubKey != null && !pubKey.equals(publicKey)) {
                 throw new NostrException(String.format("Public key mismatch. Expected %s - Received: %s", publicKey, pubKey));
             }
 
@@ -87,9 +91,11 @@ public class Nip05Validator {
         throw new NostrException(String.format("Failed to connect to %s. Error message: %s", strUrl, connection.getResponseMessage()));
     }
 
+    @SneakyThrows
     private String getPublicKey(StringBuilder content, String localPart) {
 
-        Nip05Content nip05Content = new Nip05ContentDecoder<>().decode(content.toString());
+        ObjectMapper MAPPER_AFTERBURNER = JsonMapper.builder().addModule(new AfterburnerModule()).build();
+        Nip05Content nip05Content = MAPPER_AFTERBURNER.readValue(content.toString(), Nip05Content.class);
 
         // Access the decoded data
         Map<String, String> names = nip05Content.getNames();
@@ -102,4 +108,12 @@ public class Nip05Validator {
         }
         return null;
     }
+
+    @Data
+    @AllArgsConstructor
+    public static final class Nip05Obj {
+        private String name;
+        private String nip05;
+    }
+
 }
