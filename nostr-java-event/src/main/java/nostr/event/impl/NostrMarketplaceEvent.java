05babc8ade697e0184b84ebe417ad6425c4b3a2c
erict875 <erict875@tuta.io>
2025-05-18 05:52:16 +0100

    Refactor NostrMarketplaceEvent: simplify structure, update constructor, and enhance product retrieval method

--- nostr-java-event/src/main/java/nostr/event/impl/NostrMarketplaceEvent.java
+++ nostr-java-event/src/main/java/nostr/event/impl/NostrMarketplaceEvent.java
@@ -1,92 +1,33 @@
 package nostr.event.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import nostr.base.IEvent;
 import nostr.base.PublicKey;
 import nostr.base.annotation.Event;
-import nostr.event.AbstractEventContent;
 import nostr.event.BaseTag;
-import nostr.event.IContent;
-import nostr.event.impl.CreateOrUpdateStallEvent.Stall;
-import nostr.event.json.serializer.ProductSerializer;
-import nostr.event.json.serializer.SpecSerializer;
+import nostr.event.entities.Product;
+
+import java.util.List;
 
 /**
- *
  * @author eric
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Event(name = "", nip = 15)
-public abstract class NostrMarketplaceEvent extends ParameterizedReplaceableEvent {
+@NoArgsConstructor
+public abstract class NostrMarketplaceEvent extends AddressableEvent {
 
-    protected NostrMarketplaceEvent() {
+    // TODO: Create the Kinds for the events and use it
+    public NostrMarketplaceEvent(PublicKey sender, Integer kind, List<BaseTag> tags, String content) {
+        super(sender, kind, tags, content);
     }
 
-    public NostrMarketplaceEvent(PublicKey sender, Integer kind, List<BaseTag> tags, IContent content) {
-        super(sender, kind, tags, content.toString());
+    @SneakyThrows
+    public Product getProduct() {
+        return IEvent.MAPPER_AFTERBURNER.readValue(getContent(), Product.class);
     }
-
-    @Getter
-    @Setter
-    @EqualsAndHashCode(callSuper = false)
-    @JsonSerialize(using = ProductSerializer.class)
-    public static class Product extends AbstractEventContent<NostrMarketplaceEvent> {
-
-        @JsonProperty
-        private final String id;
-
-        @JsonProperty
-        private Stall stall;
-
-        @JsonProperty
-        private String name;
-
-        @JsonProperty
-        private String description;
-
-        @JsonProperty
-        private List<String> images;
-
-        @JsonProperty
-        private String currency;
-
-        @JsonProperty
-        private Float price;
-
-        @JsonProperty
-        private int quantity;
-
-        @JsonProperty
-        private List<Spec> specs;
-
-        public Product() {
-            this.specs = new ArrayList<>();
-            this.images = new ArrayList<>();
-            this.id = UUID.randomUUID().toString();
-        }
-
-        @Data
-        @AllArgsConstructor
-        @JsonSerialize(using = SpecSerializer.class)
-        public static class Spec {
-
-            @JsonProperty
-            private final String key;
-
-            @JsonProperty
-            private final String value;
-        }
-    }
-
 }
