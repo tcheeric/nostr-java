25599d6eee78e1d57b0a4ffdfa36e88706af78a2
erict875 <erict875@tuta.io>
2025-05-18 05:43:47 +0100

    Refactor Marker class package structure to improve organization

---
+++ nostr-java-base/src/main/java/nostr/base/Marker.java
@@ -0,0 +1,29 @@
+
+package nostr.base;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ *
+ * @author squirrel
+ */
+public enum Marker {
+    ROOT("root"),
+    REPLY("reply"),
+    MENTION("mention"),
+    FORK("fork"),
+    CREATED("created"),
+    DESTROYED("destroyed"),
+    REDEEMED("redeemed");
+
+    private final String value;
+
+    Marker(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+}
