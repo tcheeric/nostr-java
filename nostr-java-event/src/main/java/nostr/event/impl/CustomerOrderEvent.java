99a52d00bba3df558a03501fa8efd9bddb2537fc
erict875 <erict875@tuta.io>
2025-05-18 05:35:59 +0100

    Refactor CheckoutEvent, CreateOrUpdateProductEvent, CreateOrUpdateStallEvent, and CustomerOrderEvent to enhance type handling, improve constructor parameters, and update validation logic

--- nostr-java-event/src/main/java/nostr/event/impl/CustomerOrderEvent.java
+++ nostr-java-event/src/main/java/nostr/event/impl/CustomerOrderEvent.java
@@ -1,22 +1,17 @@
 package nostr.event.impl;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.SneakyThrows;
+import nostr.base.IEvent;
 import nostr.base.PublicKey;
 import nostr.base.annotation.Event;
-import nostr.event.AbstractEventContent;
-import nostr.event.impl.NostrMarketplaceEvent.Product;
-import nostr.event.json.serializer.ItemSerializer;
+import nostr.event.BaseTag;
+import nostr.event.entities.CustomerOrder;
+
+import java.util.List;
 
 /**
  *
@@ -24,80 +19,20 @@ import nostr.event.json.serializer.ItemSerializer;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@Event(name = "", nip = 15)
-public class CustomerOrderEvent extends CheckoutEvent {
+@Event(name = "Customer Order Event", nip = 15)
+@NoArgsConstructor
+public class CustomerOrderEvent extends CheckoutEvent<CustomerOrder> {
 
-    private CustomerOrderEvent() {
-        super();
-    }
-    public CustomerOrderEvent(PublicKey sender, @NonNull Customer customer) {
-        super(sender, customer.getContact().getPublicKey(), customer);
+    public CustomerOrderEvent(@NonNull PublicKey sender, @NonNull List<BaseTag> tags, @NonNull String content) {
+        super(sender, tags, content, MessageType.NEW_ORDER);
     }
 
-    @Getter
-    @Setter
-    @EqualsAndHashCode(callSuper = false)
-    @ToString(callSuper = true)
-    public static class Customer extends AbstractEventContent<CheckoutEvent> {
+    @SneakyThrows
+    public CustomerOrder getCustomerOrder() {
+        return IEvent.MAPPER_AFTERBURNER.readValue(getContent(), CustomerOrder.class);
+    }
 
-        @JsonProperty
-        private final String id;
-
-        @JsonProperty
-        private MessageType type;
-
-        @JsonProperty
-        private String name;
-
-        @JsonProperty
-        private String address;
-
-        @JsonProperty
-        private String message;
-
-        @JsonProperty
-        private Contact contact;
-
-        @JsonProperty
-        private List<Item> items;
-
-        @JsonProperty("shipping_id")
-        private String shippingId;
-
-        public Customer() {
-            this.items = new ArrayList<>();
-            this.id = UUID.randomUUID().toString();
-        }
-
-        @Data
-        //@JsonSerialize(using = ContactSerializer.class)
-        public static class Contact {
-
-            @JsonProperty("nostr")
-            private final PublicKey publicKey;
-
-            @JsonProperty
-            private String phone;
-
-            @JsonProperty
-            private String email;
-
-            public Contact(@NonNull PublicKey publicKey) {
-                this.publicKey = publicKey;
-            }
-                        
-        }
-
-        @Data
-        @NoArgsConstructor
-        @JsonSerialize(using = ItemSerializer.class)
-        public static class Item {
-
-            @JsonProperty
-            private Product product;
-
-            @JsonProperty
-            private int quantity;
-        }
+    protected CustomerOrder getEntity() {
+        return getCustomerOrder();
     }
 }
