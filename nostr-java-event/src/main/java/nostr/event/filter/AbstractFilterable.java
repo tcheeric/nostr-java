baf5f61240d4b7024de586c905956c2d46aa35e8
erict875 <erict875@tuta.io>
2025-06-14 01:18:46 +0100

    Enhance AbstractFilterable: add @ToString annotation for improved string representation

--- nostr-java-event/src/main/java/nostr/event/filter/AbstractFilterable.java
+++ nostr-java-event/src/main/java/nostr/event/filter/AbstractFilterable.java
@@ -3,9 +3,11 @@ package nostr.event.filter;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.ToString;
 
 @Getter
 @EqualsAndHashCode
+@ToString
 public abstract class AbstractFilterable<T> implements Filterable {
   private final T filterable;
   private final String filterKey;
