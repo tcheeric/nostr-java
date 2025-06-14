4ebf3e3fef0177b77bf606eefe6151f369546b0c
erict875 <erict875@tuta.io>
2025-06-14 01:23:11 +0100

    Enhance IEvent: replace AfterburnerModule with BlackbirdModule for improved JSON serialization

--- nostr-java-base/src/main/java/nostr/base/IEvent.java
+++ nostr-java-base/src/main/java/nostr/base/IEvent.java
@@ -3,12 +3,13 @@ package nostr.base;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 
 /**
  *
  * @author squirrel
  */
 public interface IEvent extends IElement, IBech32Encodable {
-    ObjectMapper MAPPER_AFTERBURNER = JsonMapper.builder().addModule(new AfterburnerModule()).build();
+    ObjectMapper MAPPER_AFTERBURNER = JsonMapper.builder().addModule(new BlackbirdModule()).build();
     String getId();
 }
