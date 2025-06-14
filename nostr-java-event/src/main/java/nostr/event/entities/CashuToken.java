18c97ff2af28c5c4a67bdb32f7887e2a13b613c0
erict875 <erict875@tuta.io>
2025-06-14 01:20:28 +0100

    Enhance CashuToken: add destroyed list management and improve serialization

--- nostr-java-event/src/main/java/nostr/event/entities/CashuToken.java
+++ nostr-java-event/src/main/java/nostr/event/entities/CashuToken.java
@@ -1,19 +1,48 @@
 package nostr.event.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import nostr.event.json.serializer.CashuTokenSerializer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@JsonSerialize (using = CashuTokenSerializer.class)
 public class CashuToken {
+
+    @EqualsAndHashCode.Include
     private CashuMint mint;
+
+    @EqualsAndHashCode.Include
     private List<CashuProof> proofs;
+
+    private List<String> destroyed;
+
+    public CashuToken() {
+        this.destroyed = new ArrayList<>();
+    }
+
+    public CashuToken(@NonNull CashuMint mint, @NonNull List<CashuProof> proofs) {
+        this(mint, proofs, new ArrayList<>());
+    }
+
+    public void addDestroyed(@NonNull String eventId) {
+        this.destroyed.add(eventId);
+    }
+
+    public void removeDestroyed(@NonNull String eventId) {
+        this.destroyed.remove(eventId);
+    }
 
     public Integer calculateAmount() {
         return proofs.stream().mapToInt(CashuProof::getAmount).sum();
