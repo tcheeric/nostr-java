5f037aed09c55fd2fe223440bf225516c9cce409
erict875 <erict875@tuta.io>
2025-06-14 01:21:31 +0100

    Enhance Encoder: replace AfterburnerModule with BlackbirdModule for improved serialization

--- nostr-java-base/src/main/java/nostr/base/Encoder.java
+++ nostr-java-base/src/main/java/nostr/base/Encoder.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 
 public interface Encoder {
     ObjectMapper ENCODER_MAPPED_AFTERBURNER = JsonMapper.builder().addModule(
-                    new AfterburnerModule()).build()
+                    new BlackbirdModule()).build()
             .setSerializationInclusion(Include.NON_NULL);
 
     String encode();
