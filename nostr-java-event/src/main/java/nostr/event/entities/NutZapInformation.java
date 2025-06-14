d634721315d03afd37d576671d0914a184748d86
erict875 <erict875@tuta.io>
2025-06-14 01:24:50 +0100

    Add NutZapInformation class: define structure for relays, mints, and public key

---
+++ nostr-java-event/src/main/java/nostr/event/entities/NutZapInformation.java
@@ -0,0 +1,18 @@
+package nostr.event.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nostr.base.PublicKey;
+import nostr.base.Relay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class NutZapInformation {
+
+    public List<Relay> relays = new ArrayList<>();
+    public List<CashuMint> mints = new ArrayList<>();
+    public String p2pkPubkey;
+}
