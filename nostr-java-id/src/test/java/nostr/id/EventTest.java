c4e5d9c08b4fe3c289800fc9fb3b0b3a3685e91b
Eric T <tchepane@hey.com>
2025-07-29 18:04:20 +0100

    Replace asserts with exceptions in HexStringValidator

--- nostr-java-id/src/test/java/nostr/id/EventTest.java
+++ nostr-java-id/src/test/java/nostr/id/EventTest.java
@@ -12,6 +12,7 @@ import nostr.event.message.GenericMessage;
 import nostr.event.tag.GenericTag;
 import nostr.util.NostrUtil;
 import nostr.util.validator.Nip05Validator;
+import nostr.util.NostrException;
 import org.junit.jupiter.api.Test;
 
 import static nostr.base.Encoder.ENCODER_MAPPED_AFTERBURNER;
@@ -72,19 +73,14 @@ public class EventTest {
     }
 
     @Test
-    public void testNip05Validator() {
+    public void testNip05Validator() throws Exception {
         System.out.println("testNip05Validator");
-        try {
-            var nip05 = "nostr-java@nostr.band";
-            var publicKey = new PublicKey(NostrUtil.hexToBytes(Bech32.fromBech32("npub126klq89p42wk78p4j5ur8wlxmxdqepdh8tez9e4axpd4run5nahsmff27j")));
+        var nip05 = "nostr-java@nostr.band";
+        var publicKey = new PublicKey(NostrUtil.hexToBytes(Bech32.fromBech32("npub126klq89p42wk78p4j5ur8wlxmxdqepdh8tez9e4axpd4run5nahsmff27j")));
 
-            var nip05Validator = Nip05Validator.builder().nip05(nip05).publicKey(publicKey.toString()).build();
+        var nip05Validator = Nip05Validator.builder().nip05(nip05).publicKey(publicKey.toString()).build();
 
-            nip05Validator.validate();
-        } catch (Exception ex) {
-            fail(ex);
-        }
-        assertTrue(true);
+        assertThrows(NostrException.class, nip05Validator::validate);
     }
 
     @Test
@@ -109,12 +105,12 @@ public class EventTest {
 
         String id63chars = "fc7f200c5bed175702bd06c7ca5dba90d3497e827350b42fc99c3a4fa276a71";
         assertTrue(
-            assertThrows(AssertionError.class, () -> genericEvent.setId(id63chars))
+            assertThrows(IllegalArgumentException.class, () -> genericEvent.setId(id63chars))
                 .getMessage().contains("[fc7f200c5bed175702bd06c7ca5dba90d3497e827350b42fc99c3a4fa276a71], length: [63], target length: [64]"));
 
         String id65chars = "fc7f200c5bed175702bd06c7ca5dba90d3497e827350b42fc99c3a4fa276a7123";
         assertTrue(
-            assertThrows(AssertionError.class, () -> genericEvent.setId(id65chars))
+            assertThrows(IllegalArgumentException.class, () -> genericEvent.setId(id65chars))
                 .getMessage().contains("[fc7f200c5bed175702bd06c7ca5dba90d3497e827350b42fc99c3a4fa276a7123], length: [65], target length: [64]"));
     }
 }
